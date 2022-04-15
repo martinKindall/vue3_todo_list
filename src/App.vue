e85201ce242be6263e2399cc7b35cea8464efa3d
Martin <mart256@gmail.com>
2022-04-15 09:35:41 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="container wrapper">
+      <div class="row text-center my-2">
+        <div class="col-12">
+          <nav>
+            <RouterLink to="/">Home</RouterLink>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </header>
+  <RouterView />
+</template>
+
+<style>
+@import './assets/base.css';
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<template>
+  <div class="container">
+    <div class="row text-center my-4">
+      <div class="col-12">
+        <h2>Todo List</h2>
+      </div>
+    </div>
+  </div>
+</template>
